55670466bf6251f307af84cdb2850e355f3d2a19
Sidoine De Wispelaere <sidoine@sidoine.net>
2014-10-03 08:35:48 +0200

    Add Json attribute

---
+++ Folke.Wa/JsonAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folke.Wa
+{
+    public enum JsonUsage
+    {
+        Data,
+        Service
+    }
+
+    public class JsonAttribute : Attribute
+    {
+        public JsonUsage Usage { get; set; }
+        public bool Observable { get; set; }
+    }
+}
