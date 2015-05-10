b95c0548ce58a879e8edcc9a86238bef60e1c2f8
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-05-10 23:11:17 +0200

    Add ReturnTypeAttribute (not used ATM)

---
+++ Folke.Wa/ReturnTypeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Folke.Wa
+{
+    public class ReturnTypeAttribute
+    {
+        public Type ReturnType { get; set; }
+
+        public ReturnTypeAttribute(Type returnType)
+        {
+            ReturnType = returnType;
+        }
+    }
+}
